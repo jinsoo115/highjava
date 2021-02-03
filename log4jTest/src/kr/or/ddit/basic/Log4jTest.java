3d56b13ef8f6272d10c4b966e51c4d71815c3b72
jinsoo115 <wlstn115@naver.com>
2021-02-03 17:24:03 +0900

    0203

--- log4jTest/src/kr/or/ddit/basic/Log4jTest.java
+++ log4jTest/src/kr/or/ddit/basic/Log4jTest.java
@@ -1,5 +1,6 @@
 package kr.or.ddit.basic;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class Log4jTest {
@@ -10,11 +11,21 @@ public class Log4jTest {
 		// 메서드를 제공한다.
 		// 형식) logger객체 변수, 로그레벨메서드("출력할 메시지");
 		// 로그레벨 : trace < debug < info < warn < error < fatal
-		logger.trace("이것은 log4j의 Trace 레벨의 메시지입니다.");
-		logger.debug("이것은 log4j의 Debug 레벨의 메시지입니다.");
-		logger.info("이것은 log4j의 Info 레벨의 메시지입니다.");
-		logger.warn("이것은 log4j의 Warn 레벨의 메시지입니다.");
-		logger.error("이것은 log4j의 Error 레벨의 메시지입니다.");
-		logger.fatal("이것은 log4j의 Fatal 레벨의 메시지입니다.");
+		
+//		logger.trace("이것은 log4j의 Trace 레벨의 메시지입니다.");
+//		logger.debug("이것은 log4j의 Debug 레벨의 메시지입니다.");
+//		logger.info("이것은 log4j의 Info 레벨의 메시지입니다.");
+//		logger.warn("이것은 log4j의 Warn 레벨의 메시지입니다.");
+//		logger.error("이것은 log4j의 Error 레벨의 메시지입니다.");
+//		logger.fatal("이것은 log4j의 Fatal 레벨의 메시지입니다.");
+		
+		//---------------------------------------------------
+		// 형식2) logger객체변수.log(Level.로그레벨, "출력할 메시지")
+		logger.log(Level.TRACE, "이것은 log4j의 Trace 레벨의 메시지입니다.");
+		logger.log(Level.DEBUG, "이것은 log4j의 Debug 레벨의 메시지입니다.");
+		logger.log(Level.INFO, "이것은 log4j의 Info 레벨의 메시지입니다.");
+		logger.log(Level.WARN, "이것은 log4j의 Warn 레벨의 메시지입니다.");
+		logger.log(Level.ERROR, "이것은 log4j의 Error 레벨의 메시지입니다.");
+		logger.log(Level.FATAL, "이것은 log4j의 Fatal 레벨의 메시지입니다.");
 	}
 }
